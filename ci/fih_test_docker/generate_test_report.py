8cb0e237a5741a74df68b6a445b27376a0ddf96a
Roland Mikhel <roland.mikhel@arm.com>
2023-02-15 16:20:24 +0100

    Refactor validation and parsing logic
    Signed-off-by: Roland Mikhel <roland.mikhel@arm.com>

--- ci/fih_test_docker/generate_test_report.py
+++ ci/fih_test_docker/generate_test_report.py
@@ -13,48 +13,13 @@
 # limitations under the License.
 
 import argparse
-import yaml
-import collections
-import os
+from utils import CATEGORIES, parse_yaml_file
 
-CATEGORIES = {
-        'TOTAL': 'Total tests run',
-        'SUCCESS': 'Tests executed successfully',
-        'FAILED': 'Tests failed to execute successfully',
-        # the execution never reached the address
-        'ADDRES_NOEXEC': 'Address was not executed',
-        # The address was successfully skipped by the debugger
-        'SKIPPED': 'Address was skipped',
-        'NO_BOOT': 'System not booted (desired behaviour)',
-        'BOOT': 'System booted (undesired behaviour)'
-}
 
 
 def print_results(results):
-    test_stats = collections.Counter()
-    failed_boot_last_lines = collections.Counter()
-    exec_fail_reasons = collections.Counter()
 
-    for test in results:
-        test = test["skip_test"]
-
-        test_stats.update([CATEGORIES['TOTAL']])
-
-        if test["test_exec_ok"]:
-            test_stats.update([CATEGORIES['SUCCESS']])
-
-            if "skipped" in test.keys() and not test["skipped"]:
-                # The debugger didn't stop at this address
-                test_stats.update([CATEGORIES['ADDRES_NOEXEC']])
-                continue
-
-            if test["boot"]:
-                test_stats.update([CATEGORIES['BOOT']])
-                continue
-
-            failed_boot_last_lines.update([test["last_line"]])
-        else:
-            exec_fail_reasons.update([test["test_exec_fail_reason"]])
+    (test_stats,failed_boot_last_lines,exec_fail_reasons) = results
 
     print("{:s}: {:d}.".format(CATEGORIES['TOTAL'], test_stats[CATEGORIES['TOTAL']]))
     print("{:s} ({:d}):".format(CATEGORIES['SUCCESS'], test_stats[CATEGORIES['SUCCESS']]))
@@ -69,12 +34,6 @@ def print_results(results):
     for reason in exec_fail_reasons.keys():
         print("    {:s} ({:d})".format(reason, exec_fail_reasons[reason]))
 
-    # If there are any successful boots (undesired behaviours), fail the job
-    skip_size = os.getenv("SKIP_SIZE")
-    fih_level = os.getenv("FIH_LEVEL")
-    print(f"{fih_level}")
-    if(test_stats[CATEGORIES['BOOT']] > 0 and skip_size == "2,4,6" and fih_level != ""):
-        exit(1)
 
 
 
@@ -83,15 +42,8 @@ def main():
     parser.add_argument('filename', help='yaml file to process')
 
     args = parser.parse_args()
-
-    with open(args.filename) as output_yaml_file:
-        results = yaml.safe_load(output_yaml_file)
-
-        if results:
-            print_results(results)
-        else:
-            print("Failed to parse output yaml file.")
-
+    results = parse_yaml_file(args.filename)
+    print_results(results)
 
 if __name__ == "__main__":
     main()
